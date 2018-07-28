c27ec8d9739561740aa0a25ab12f4ea74c1c7ffb
debao <debao@debaodeiMac.local>
2018-07-28 11:55:23 +0800

    还原项目

--- TexaspokerCalculator/wsgi_test.py
+++
@@ -1,4 +0,0 @@
-def application(env, start_response):
-        start_response('200 OK',[('Content-Type', 'text/html')])
-        #return ['Hello world'] # Python2
-        return [b'Hello world'] # Python3
--- holdem/apps.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
-from django.apps import AppConfig
-
-
-class HoldemConfig(AppConfig):
-    name = 'holdem'
-    verbose_name = u'微信小程序德州扑克计算器'
